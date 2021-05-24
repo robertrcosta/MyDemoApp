c88e62de5427177cc4899056e3b6633940e82d34
RobertRC <robert.rodriguez@sunwebgroup.com>
2021-05-24 18:06:48 +0200

    Added project and basic DataController

---
+++ MyDemoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyDemoApp
+//
+//  Created by Robert Rodriguez on 23/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyDemoApp/MyDemoAppApp.swift
@@ -0,0 +1,26 @@
+//
+//  MyDemoAppApp.swift
+//  MyDemoApp
+//
+//  Created by Robert Rodriguez on 23/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct MyDemoAppApp: App {
+    @StateObject var dataController: DataController
+
+    init() {
+        let dataController = DataController()
+        _dataController = StateObject(wrappedValue: dataController)
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+                .environmentObject(dataController)
+        }
+    }
+}
