a96f4743c8f566aa21def8c452a23aa930539d48
RobertRC <robert.rodriguez@sunwebgroup.com>
2021-05-31 07:52:20 +0200

    Added tabView & CoreData Helpers

---
+++ MyDemoApp/HomeView.swift
@@ -0,0 +1,33 @@
+//
+//  HomeView.swift
+//  MyDemoApp
+//
+//  Created by Robert Rodriguez on 24/05/2021.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @EnvironmentObject var dataController: DataController
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Button("Add Data") {
+                    dataController.deleteAll()
+                    try? dataController.createSampleData()
+                }
+            }
+            .navigationTitle("Home")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            HomeView()
+        }
+    }
+}
